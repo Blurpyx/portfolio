3d8172dda9b68054abd64b3dd78b5728252be6c8
Tommy Tytgat <tommytytgat.tt@gmail.com>
2024-09-20 13:16:25 +0200

    Navbar start template

--- portfolio-ui/src/App.tsx
+++ portfolio-ui/src/App.tsx
@@ -1,10 +1,10 @@
-import './App.css';
+import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import Intro from "./Components/Intro/Intro";
 
 function App() {
   return (
-    <div className='app-container'>
+    <div className="app-container">
       <Navbar />
       <Intro />
     </div>
--- portfolio-ui/src/Components/Navbar/Navbar.tsx
+++ portfolio-ui/src/Components/Navbar/Navbar.tsx
@@ -1,22 +1,70 @@
-import './Navbar.css';
+import "./Navbar.css";
+import LinkedIn from "../../images/icons/linkedin.png";
+import Facebook from "../../images/icons/facebook.png";
+import Instagram from "../../images/icons/instagram.png";
+import Twitter from "../../images/icons/twitter.png";
 
 export default function Navbar() {
-    return (
-        <nav className="w-full flex justify-end text-white top-0 right-0 pt-2 pr-2">
-            <ul className="flex">
-                <li>
-                    <a href="/" className="px-2 active">Home</a>
-                </li>
-                <li>
-                    <a href="#about" className="px-2">About me</a>
-                </li>
-                <li>
-                    <a href="#skills" className="px-2">Skills</a>
-                </li>
-                <li>
-                    <a href="#contact" className="px-4">Talk to me</a>
-                </li>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className="w-full absolute top-0 h-20">
+      <div className="flex grow h-full border-b border-gray-200">
+        <a href="/" className="ml-8 mr-10">
+          <h1 className="leading-[80px]">Portfolio</h1>
+        </a>
+        <span className="flex grow leading-[80px]">
+          <span className="px-10 border-x">
+            <span>Tytgat Tommy</span>
+          </span>
+          <ul className="flex justify-between grow px-10 border-r">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <a href="#skills">Skills</a>
+            </li>
+            <li>
+              <a href="#contact">Reach out</a>
+            </li>
+          </ul>
+          <ul className="flex px-10 items-center">
+            <li>
+              <a
+                href="https://www.facebook.com/profile.php?id=100009888813009"
+                target="blank"
+              >
+                <img src={LinkedIn} alt="LinkedIn" className="w-9 mx-2"/>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.facebook.com/profile.php?id=100009888813009"
+                target="blank"
+              >
+                <img src={Facebook} alt="Facebook" className="w-10 mx-1"/>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.facebook.com/profile.php?id=100009888813009"
+                target="blank"
+              >
+                <img src={Instagram} alt="Instagram" className="w-8 mx-2"/>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.facebook.com/profile.php?id=100009888813009"
+                target="blank"
+              >
+                <img src={Twitter} alt="Twitter" className="w-8 mx-2"/>
+              </a>
+            </li>
+          </ul>
+        </span>
+      </div>
+    </nav>
+  );
 }
