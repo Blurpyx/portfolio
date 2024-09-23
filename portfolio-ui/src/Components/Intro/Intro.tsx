920e98f2446ecc136cf76cd98886ec7616ee813f
Tommy Tytgat <tommytytgat.tt@gmail.com>
2024-09-23 09:54:44 +0200

    Intro decoration

--- portfolio-ui/src/Components/Intro/Intro.tsx
+++ portfolio-ui/src/Components/Intro/Intro.tsx
@@ -1,9 +1,11 @@
 import "./Intro.css";
+import Decoration from "../../images/icons/spirals-of-vines.png"
 
 export default function Intro() {
   return (
-    <div>
-      
+    <div className="w-full mt-20 intro">
+      <h1 className="w-3/5 mx-auto py-48">Hi! Feast your eyes on all my projects and decide whether I'm a good match for your business.</h1>
+      <img src={Decoration} alt="" className="w-10 mx-auto" />
     </div>
   );
 }
